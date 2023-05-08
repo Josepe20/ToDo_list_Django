5b9d14d1c126e965fd6fcf8fba030149bbb7fc69
Jose Chay <chepechay16@gmail.com>
2023-05-08 15:08:46 -0600

    commit 2, form and taks were added to database

--- task/views.py
+++ task/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def list_task(request):
-    return render(request, 'list_task.html')
+    return render(request, 'task/list_task.html')
+
+def create_task(request):
+    print(request.POST)
+    task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    return redirect('/tasks/')
 
