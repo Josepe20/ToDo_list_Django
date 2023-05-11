0fb2a95b5028e4d470833d375b51be677a9c4c95
Jose Chay <chepechay16@gmail.com>
2023-05-11 09:38:20 -0600

    some urls changes

--- task/views.py
+++ task/views.py
@@ -10,7 +10,7 @@ def create_task(request):
     print(request.POST)
     task = Task(title=request.POST['title'], description=request.POST['description'])
     task.save()
-    return redirect('/tasks/')
+    return redirect('/')
 
 def update_page(request, task_id):
     task = Task.objects.get(id=task_id)
@@ -23,9 +23,9 @@ def update_task(request, task_id):
     task.description = request.POST['description']
     task.save()
 
-    return redirect('/tasks/')
+    return redirect('/')
 
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
-    return redirect('/tasks/')
+    return redirect('/')
