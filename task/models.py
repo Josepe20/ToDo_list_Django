1023bc124689d41a2e2b0fdd47b0573db5d53917
Jose Chay <chepechay16@gmail.com>
2023-06-09 00:05:11 -0600

    database updated

--- task/models.py
+++ task/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.timezone import timezone
+from django.utils import timezone
 
-# Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)  # Utiliza auto_now_add para la fecha de creación
     datecompleted = models.DateTimeField(null=True)
     important = models.BooleanField(default=False)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Renombra el campo a "user"
 
+    def __str__(self):
+        return self.title
