480fadb89ca60c9ed4510afa9662f64bbe1bc4cb
Jose Chay <chepechay16@gmail.com>
2023-06-12 10:29:27 -0600

    complete task and filtering task completed

--- task/urls.py
+++ task/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import list_task, create_task, delete_task, update_task, singup, singout, singin
+from .views import list_task, create_task, delete_task, update_task, singup, singout, singin, complete_task, list_task_completed, home
 
 urlpatterns = [
-    path('', list_task),
+    path('', home),
+    path('task_list/', list_task, name='tasks_uncompleted'),
+    path('tasks_completed/', list_task_completed, name='tasks_completed'),
     path('singup/', singup, name='singup'),
     path('logout/', singout, name='logout'),
     path('login/', singin, name='login'),
     path('new_task/', create_task, name='create_task'),
-    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
     path('update_task/<int:task_id>/', update_task, name='update_task'),
+    path('complete_task/<int:task_id>/', complete_task, name='complete_task'),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect, resolve_url
+from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from django.utils import timezone
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -67,12 +68,25 @@ def singout(request):
 
 
 # Create your views here.
+def home(request):
+    return render(request, 'task/list_task.html')
+
 def list_task(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     user = request.user
     return render(request, 'task/list_task.html', {
         'tasks': tasks,
         'user_name': user,
+        'completed': False,
+    })
+
+def list_task_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False)
+    user = request.user
+    return render(request, 'task/list_task.html', {
+        'tasks': tasks,
+        'user_name': user,
+        'completed': True,
     })
 
 
@@ -98,23 +112,45 @@ def create_task(request):
 
 
 def update_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    form = TaskForm(instance=task)
 
     if request.method == 'POST':
-        task.title = request.POST['title']
-        task.description = request.POST['description']
-        task.save()
+        try:
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('/')
 
+        except ValueError:
+            return render(request, 'task/task_form.html', {
+                'task': task,
+                'form': form,
+                'operation': 'update',
+                "error": "Error updating task"
+            })
+
+    return render(request, 'task/task_form.html', {
+        'task': task,
+        'form': form,
+        'operation': 'update'
+    })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+
+    if request.method == "POST":
+        task.datecompleted = timezone.now()
+        task.save()
         return redirect('/')
 
     return render(request, 'task/task_form.html', {
         'task': task,
-        'form': TaskForm,
-        'operation': 'update'
+        'operation': 'complete',
     })
 
 def delete_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
 
     if request.method == 'POST':
         task.delete()
@@ -122,6 +158,5 @@ def delete_task(request, task_id):
 
     return render(request, 'task/task_form.html', {
         'task': task,
-        'form': TaskForm,
-        'operation': 'delete'
+        'operation': 'delete',
     })
