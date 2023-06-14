235813a1a84bd63d81340374860771d84316258f
Jose Chay <chepechay16@gmail.com>
2023-06-14 14:19:09 -0600

    deploy settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 # This setting tells Django at which URL static files are going to be served to the user.
 # Here, they well be accessible at your-domain.onrender.com/static/...
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
